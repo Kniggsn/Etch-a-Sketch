760f53450dee6af5e455dcace9e47eb34607a75c
Kniggsn <65507312+Kniggsn@users.noreply.github.com>
2024-01-29 21:03:12 +0100

    Add color selection and clear button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 const mainContainer = document.querySelector(".main-container");
 const body = document.querySelector("body");
-console.log(body);
-let mouseState = 0;
+const clearBtn = document.querySelector("#clear-btn");
+const cells = [];
 
-let numberOfColumns = 8;
-let numberOfRows = 8;
+let mouseState = 0;
+let selectedColor = "black";
+let selectedColorDiv = document.querySelector(".black");
+
+let numberOfColumns = 16;
+let numberOfRows = 16;
 
 body.addEventListener("mouseup", () => mouseState = 0);
 body.addEventListener("mouseleave", () => mouseState = 0);
+clearBtn.addEventListener("click", () => createGrid());
+document.querySelectorAll(".right-buttons-container").forEach(element => element.addEventListener("click", changeSelectedColor));
 
 createGrid();
 
 function createGrid(){
+    deleteGrid();
     for (let i=0; i<numberOfColumns; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row");
         rowContainer.classList.add("row" + i);
-        rowContainer.style.height = String(960/numberOfRows) + "px";
+        rowContainer.style.height = String(600/numberOfRows) + "px";
         rowContainer.draggable = false;
         for (let j=0; j<numberOfColumns; j++) {
             const cell = document.createElement("div");
@@ -27,7 +34,6 @@ function createGrid(){
             cell.addEventListener("mousedown", () => mouseDown(cell));
             cell.addEventListener("mouseover", () => mouseOver(cell));
             cell.draggable = false;
-
         mainContainer.appendChild(rowContainer);
     }
 }
@@ -49,13 +55,28 @@ function setBorderThickness(i, j, cell) {
 }
 
 function mouseDown(c) {
-    c.style.backgroundColor = "black";
+    c.style.backgroundColor = selectedColor;
     mouseState = 1;
 }
 
 function mouseOver(c) {
     if (mouseState === 1) {
-        c.style.backgroundColor = "black";
+        c.style.backgroundColor = selectedColor;
     }
 }
 
+function changeSelectedColor(element) {
+    selectedColorDiv.style.borderWidth = "1px";
+    selectedColor = window.getComputedStyle(element.target).getPropertyValue("background-color");
+    element.target.style.borderWidth = "3px";
+    selectedColorDiv = element.target;
+}
+
+function deleteGrid() {
+    rows = document.querySelectorAll(".row");
+    if (rows[0]){
+        rows.forEach(element => {
+            element.remove();
+        });
+    };
+}
