d6761d80aa914f6315bf0be2af354ebb9553e36a
Kniggsn <65507312+Kniggsn@users.noreply.github.com>
2024-02-05 18:13:49 +0100

    Tidy up the code a bit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-const gridContainer = document.querySelector("#grid-container");
 const body = document.querySelector("body");
+const gridContainer = document.querySelector("#grid-container");
 const clearBtn = document.querySelector("#clear-btn");
 const showGridBtn = document.querySelector("#show-grid-btn");
 const autoDrawBtn = document.querySelector("#auto-draw-btn");
 const slider = document.querySelector(".slider");
 const colorPicker = document.querySelector("#color-picker");
 const colorChangeButtons = document.querySelectorAll(".color-change-buttons");
+const gridContainerWidth = window.getComputedStyle(gridContainer).width;
 
 const activeButtonColor = "rgb(0, 20, 46)";
 const inactiveButtonColor = "#352f44";
 
-document.querySelector("#hidden-btn").addEventListener("click", (event) => event.target.style.display = "none");
-
 let mouseState = 0;
 let selectedColor = "black";
-let selectedColorDiv = document.querySelector(".black");
+
 
 let numberOfColumns = 16;
 let numberOfRows = 16;
@@ -39,8 +38,8 @@ colorChangeButtons.forEach((button) => button.addEventListener("click", changeBu
 colorPicker.addEventListener("input", changeSelectedColor, false);
 colorPicker.value = "#000000";
 
-setUpSlider();
 createGrid();
+setUpSlider();
 
 function createGrid() {
     deleteGrid();
@@ -50,8 +49,7 @@ function createGrid() {
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row");
         rowContainer.classList.add("row" + i);
-        rowContainer.style.height = String(450 / numberOfRows) + "px";
-        rowContainer.draggable = false;
+        rowContainer.style.height = String(gridContainerWidth.split("p")[0] / numberOfRows) + "px";
         for (let j = 0; j < numberOfColumns; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
@@ -59,46 +57,12 @@ function createGrid() {
             rowContainer.appendChild(cell);
             cell.addEventListener("mousedown", changeColor);
             cell.addEventListener("mouseover", changeColor);
-            cell.draggable = false;
             if (showGrid) {
                 cell.style.borderStyle = "solid";
             }
             gridContainer.appendChild(rowContainer);
         }
     }
-}
-
-function changeColor(cell) {
-    if (!autoDraw && (cell.type === "mouseover" && !mouseDown)) {
-        return
-    }
-    if (mode === "defaultColorMode") {
-        cell.target.style.backgroundColor = selectedColor;
-    } else if (mode === "eraserMode") {
-        cell.target.style.backgroundColor = "white";
-    } else if (mode === "rainbowMode") {
-        cell.target.style.backgroundColor = `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
-    } else if (mode === "greyscaleMode") {
-        let rgb = window.getComputedStyle(cell.target).backgroundColor;
-        rgb = rgb.substring(4, rgb.length-1)
-                .replace(/ /g, '')
-                .split(',');
-        if(rgb[0] === rgb[1] && rgb[1] === rgb[2]) {
-            rgb[0] = rgb[0] - 10;
-            rgb[1] = rgb[1] - 10;
-            rgb[2] = rgb[2] - 10;
-        } else {
-            rgb[0] = 245;
-            rgb[1] = 245;
-            rgb[2] = 245;
-        }
-        cell.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-    }
-
-}
-
-function changeSelectedColor(event) {
-    selectedColor = event.target.value;
 }
 
 function deleteGrid() {
@@ -114,25 +78,52 @@ function setUpSlider() {
     const output = document.getElementById("slider-output");
     slider.value = 16 / 0.64;
     output.innerHTML = Math.round(slider.value * 0.64);
-    slider.oninput = function () {
+    slider.oninput = function() {
         output.innerHTML = Math.round(this.value * 0.64);
     }
 }
 
+function changeColor(cell) {
+    if (!autoDraw && (cell.type === "mouseover" && !mouseDown)) {
+        return
+    }
+    switch(mode) {
+        case "defaultColorMode" : 
+            cell.target.style.backgroundColor = selectedColor;
+            break;
+        case "eraserMode": 
+            cell.target.style.backgroundColor = "white";
+            break;
+        case "rainbowMode": 
+            cell.target.style.backgroundColor = `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
+            break;
+        case "greyscaleMode":         
+            let rgb = window.getComputedStyle(cell.target).backgroundColor;
+            rgb = calculateGreyScale(rgb);
+            cell.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+            break;
+        }
+}
+
 function changeColorMode(event) {
-    if (event.target.id === "color-btn") {
-        mode = "defaultColorMode";
-    } else if (event.target.id === "eraser-btn") {
-        mode = "eraserMode";
-    } else if (event.target.id === "rainbow-btn") {
-        mode = "rainbowMode";
-    } else if (event.target.id === "greyscale-btn") {
-        mode = "greyscaleMode";
+    switch(event.target.id) {
+        case "color-btn":
+            mode = "defaultColorMode";
+            break;
+        case "eraser-btn":
+            mode = "eraserMode";
+            break;
+        case "rainbow-btn":
+            mode = "rainbowMode";
+            break;
+        case "greyscale-btn":
+            mode = "greyscaleMode";
+            break;
     }
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function changeSelectedColor(event) {
+    selectedColor = event.target.value;
 }
 
 function changeButtonBackgroundColor(event) {
@@ -143,7 +134,6 @@ function changeButtonBackgroundColor(event) {
 function toggleGrid() {
     let cells = document.querySelectorAll(".cell");
     if (showGrid) {
-        console.log("test");
         cells.forEach((cell) => cell.style.borderStyle = "none");
         showGrid = false;
         showGridBtn.style.backgroundColor = inactiveButtonColor;
@@ -162,4 +152,23 @@ function toggleAutoDraw() {
         autoDraw = true;
         autoDrawBtn.style.backgroundColor = activeButtonColor;
     }
+}
+
+function calculateGreyScale(rgb) {
+    rgb = rgb.substring(4, rgb.length-1)
+    .replace(/ /g, '')
+    .split(',');
+    if(rgb[0] === rgb[1] && rgb[1] === rgb[2]) {
+        rgb[0] = rgb[0] - 25;
+        rgb[1] = rgb[1] - 25;
+        rgb[2] = rgb[2] - 25;
+    } else {
+        rgb[0] = 245;
+        rgb[1] = 245;
+        rgb[2] = 245;
+    } return rgb;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
